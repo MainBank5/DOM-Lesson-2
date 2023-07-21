146fab77b15397d0d5f40190cae97c994d606857
eliudkaruga <eliudkaruga97@gmail.com>
2023-07-21 17:16:33 +0300

    end of dom

--- app.js
+++ app.js
@@ -84,3 +84,75 @@ title.innerHTML = 'Favorite Movies';
 document.querySelector('#wrapper').appendChild(title)
 
 console.log(title)
+
+//removefirstchild
+
+function removeFirstItem () {
+    //select the parent and then the child which you want to remove
+    const ul = document.querySelector('.list-items');
+    const li = document.querySelector('li:nth-child(2)');
+
+    ul.removeChild(li);
+}
+
+//removeFirstItem();
+
+//alternative way of removing any item you would want
+//your function argument will be the item number you want to remove
+function removeAnother (itemNumber) {
+    const ul = document.querySelector('.list-items');
+    const li = document.querySelector(`li:nth-child(${itemNumber})`);
+
+    ul.removeChild(li);
+}
+
+//removeAnother(2)
+
+function removeAnother2 (itemNumber) {
+    const ul = document.querySelector('.list-items');
+    const li = document.querySelectorAll('li') [itemNumber - 1];
+
+    ul.removeChild(li);
+}
+
+//removeAnother2(2)
+
+//Styles and classes 
+const para = document.querySelector('p');
+const myItems = document.querySelector('.list-items');
+const myMovies = document.querySelectorAll('li');
+
+function run() {
+   //className
+   console.log(myItems.className);
+   //para.className = 'card dark' //'dark' alone overwrites the existing classname in the html file, consequently the event
+   //triggers the styling of class.To maintain the original class name, simply include it in the 'new classname'
+
+   //classList 
+   //console.log(myItems.classList) //this display a domtokenlist which is basically an array of the className, and so its iteratable 
+   for (let i=0 ;i<myItems.classList.length;i++) {
+    console.log("Class Name : " + myItems.classList[i]);
+   }
+
+   //para.classList.add('dark') //it adds the new class and still mantains the original one
+   //para.classList.remove('card');
+
+   //para.classList.toggle('dark')
+   para.classList.replace('card', 'dark');
+
+   //the typical styling
+   para.style.color = 'red'
+   myItems.style.lineHeight = '5'
+
+   myMovies.forEach((item, index) => {
+    item.style.color ='red'
+
+    if(index === 2) {
+        item.style.color = 'yellow'
+        item.textContent += ' -stytled'
+    }
+   })
+
+}
+
+document.querySelector('button').onclick = run;
